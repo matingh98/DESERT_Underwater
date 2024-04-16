cf3c603a43c905aabbbcfccaa3ad640b6c34fe1f
Matin Ghalkhani <matinghalkhani2@gmail.com>
2024-04-16 11:39:47 +0200

    The first Compilation of DSync

--- DESERT_Framework/DESERT/synchronization/uwdsync/initlib.cpp
+++ DESERT_Framework/DESERT/synchronization/uwdsync/initlib.cpp
@@ -27,149 +27,18 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /**
- * @file   data_link/uwpolling/initlib.cpp
- * @author Federico Favaro
+ * @file   synchronization/uwdsync/initlib.cpp
+ * @author Matin Ghalkhani
  * @version 1.0.0
  *
  * \brief Provides the initialization of the uwpolling libraries
  *
  */
-
 #include <tclcl.h>
-#include "uwpolling_AUV.h"
-#include "uwpolling_NODE.h"
-#include "uwpolling_SINK.h"
-#include "uwpolling_cmn_hdr.h"
-#include "sap.h"
-#include "packet.h"
-
-int hdr_PROBE::offset_ = 0;
-int hdr_TRIGGER::offset_ = 0;
-int hdr_POLL::offset_ = 0;
-int hdr_AUV_MULE::offset_ = 0;
-int hdr_ACK_SINK::offset_ = 0;
-int hdr_PROBE_SINK::offset_ = 0;
-
-packet_t PT_TRIGGER;
-packet_t PT_PROBE;
-packet_t PT_PROBE_SINK;
-packet_t PT_POLL;
-packet_t PT_AUV_MULE;
-packet_t PT_ACK_SINK;
-
-/**
- * Class that describe the Header of PROBE Packet
- */
-static class ProbeHeaderClass : public PacketHeaderClass
-{
-public:
-	/**
-	 * Constructor of the class
-	 */
-	ProbeHeaderClass()
-		: PacketHeaderClass("PacketHeader/PROBE", sizeof(hdr_PROBE))
-	{
-		this->bind();
-		bind_offset(&hdr_PROBE::offset_);
-	}
-} class_hdr_PROBE;
-
-/**
- * Class that describe the Header of TRIGGER Packet
- */
-static class TriggerHeaderClass : public PacketHeaderClass
-{
-public:
-	/**
-	 * Constructor of the class
-	 */
-	TriggerHeaderClass()
-		: PacketHeaderClass("PacketHeader/TRIGGER", sizeof(hdr_TRIGGER))
-	{
-		this->bind();
-		bind_offset(&hdr_TRIGGER::offset_);
-	}
-} class_hdr_TRIGGER;
-
-/**
- * Class that describe the Header of POLL Packet
- */
-static class PollHeaderClass : public PacketHeaderClass
-{
-public:
-	/**
-	 * Constructor of the class
-	 */
-	PollHeaderClass()
-		: PacketHeaderClass("PacketHeader/POLL", sizeof(hdr_POLL))
-	{
-		this->bind();
-		bind_offset(&hdr_POLL::offset_);
-	}
-} class_hdr_POLL;
-
-/**
- * Class that describe the Header of AUV_MULEt
- */
-static class AuvMuleHeaderClass : public PacketHeaderClass
-{
-public:
-	/**
-	 * Constructor of the class
-	 */
-	AuvMuleHeaderClass()
-		: PacketHeaderClass("PacketHeader/AUV_MULE", sizeof(hdr_AUV_MULE))
-	{
-		this->bind();
-		bind_offset(&hdr_AUV_MULE::offset_);
-	}
-} class_hdr_AUV_MULE;
-
-/**
- * Class that describe the Header of ACK sent by the sink
- */
-static class AckSinkHeaderClass : public PacketHeaderClass
-{
-public:
-	/**
-	 * Constructor of the class
-	 */
-	AckSinkHeaderClass()
-		: PacketHeaderClass("PacketHeader/ACK_SINK", sizeof(hdr_ACK_SINK))
-	{
-		this->bind();
-		bind_offset(&hdr_ACK_SINK::offset_);
-	}
-} class_hdr_ACK_SINK;
-
-/**
- * Class that describe the Header of PROBE_SINK Packet
- */
-static class ProbeSinkHeaderClass : public PacketHeaderClass
-{
-public:
-	/**
-	 * Constructor of the class
-	 */
-	ProbeSinkHeaderClass()
-		: PacketHeaderClass("PacketHeader/PROBE_SINK", sizeof(hdr_PROBE_SINK))
-	{
-		this->bind();
-		bind_offset(&hdr_PROBE_SINK::offset_);
-	}
-} class_hdr_PROBE_SINK;
-
-extern EmbeddedTcl uwpolling_default;
-
+extern EmbeddedTcl UwDSyncTclCode;
 extern "C" int
-Uwpolling_Init()
+Uwdsync_Init()
 {
-	PT_PROBE = p_info::addPacket("UWPOLLING/PROBE");
-	PT_TRIGGER = p_info::addPacket("UWPOLLING/TRIGGER");
-	PT_POLL = p_info::addPacket("UWPOLLING/POLL");
-	PT_AUV_MULE = p_info::addPacket("UWPOLLING/AUV_MULE");
-	PT_ACK_SINK =p_info::addPacket("UWPOLLING/ACK_SINK");
-	PT_PROBE_SINK = p_info::addPacket("UWPOLLING/PROBE_SINK");
-	uwpolling_default.load();
-	return 0;
+UwDSyncTclCode.load();
+return 0;
 }
